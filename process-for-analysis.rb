b7a97a68b5e3f30ffb84009d703c36c7774582d5
sanryuu <cpu.cpu.cpu9@gmail.com>
2013-09-08 01:49:46 +0900

    複数のメソッド名の追加
    直近で追加する予定のメソッドのメソッド名の追加をした。

--- process-for-analysis.rb
+++ process-for-analysis.rb
@@ -4,13 +4,16 @@
 # データ分析に用いるデータの加工用ライブラリ
 
 # 読み込み用メソッド(配列に入れるだけ)
+# input_csv(file_path)
 
 # ヘッダaliasメソッド
 # 引数 alias(天気 => weather, 日付 => date)を与えて別名を付ける
+# alias_header(alias_array)
 
 # ヘッダ追加メソッド
 
 # データ判別メソッド(yyyy/mm/dd)で始まるものをデータにするなど
+# judge_need(regular_expression)
 
 # 要、不要メソッド(0=>true, 1=>false)
 # 必要なものをtrueとして、不要なデータを削る
